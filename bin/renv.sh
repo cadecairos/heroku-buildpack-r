bd7c20754585a984e528a7c3af30b64403262905
Chris DeCairos <cade@mozillafoundation.org>
2018-04-26 14:39:51 -0400

    debug

--- bin/renv.sh
+++ bin/renv.sh
@@ -7,3 +7,4 @@ FAKEROOT_DIR="$TOOLS_DIR/fakeroot"
 FAKECHROOT_DIR="$TOOLS_DIR/fakechroot"
 export PATH="$FAKEROOT_DIR/bin:$FAKECHROOT_DIR/sbin:$FAKECHROOT_DIR/bin:./bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:$PATH"
 export PKG_CONFIG_PATH="$APP_DIR/.root/usr/lib/x86_64-linux-gnu/pkgconfig"
+echo $PKG_CONFIG_PATH
